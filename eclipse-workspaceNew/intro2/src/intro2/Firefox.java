cb1e0dc335a1b855f6033dbde39f05af6b33dbbd
lgutro <38503093+lgutro@users.noreply.github.com>
2018-10-31 20:52:29 -0400

    Add test projects
    Initial commit

---
+++ eclipse-workspaceNew/intro2/src/intro2/Firefox.java
@@ -0,0 +1,22 @@
+package intro2;
+
+/* import org.openqa.selenium.By; */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.gecko.driver", "c:\\users\\lgutr\\downloads\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		
+		 
+		driver.get("http://www.profilemotel.com");
+		System.out.println(driver.getTitle()); 
+		System.out.println(driver.getCurrentUrl());
+	
+	}
+
+}
---
+++ eclipse-workspaceNew/intro2/src/intro2/IExplorer.java
@@ -0,0 +1,27 @@
+package intro2;
+
+/* import org.openqa.selenium.By; */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class IExplorer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.ie.driver", "c:\\users\\lgutr\\downloads\\MicrosoftWebDriver.exe");
+		WebDriver driver=new InternetExplorerDriver();
+		
+
+
+		
+		driver.get("http://www.profilemotel.com");
+		System.out.println(driver.getTitle()); 	
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getPageSource()); 
+		driver.quit();
+		
+		
+	}
+
+}
