7011a379549efa7a704bbba3e6c16689ac5b860c
lgutro <38503093+lgutro@users.noreply.github.com>
2018-11-06 10:37:12 -0500

    How to exclude a test within a suite

---
+++ eclipse-workspaceNew/TestNGTutorial/src/test/day3.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day3 {
+	@Test
+	public void WebLoginCarLoan() 
+	{
+		//selenium	
+		System.out.println("weblogincar");
+	}
+
+	@Test
+	public void MobileLoginCarLoan() 
+	{
+		//Appium
+		System.out.println("mobilelogincar");
+
+	}
+	
+	@Test
+	public void LoginAPICarLoan()
+	{
+		// Rest API 
+
+		System.out.println("restlogincar");
+}
+}
---
+++ eclipse-workspaceNew/TestNGTutorial/src/test/day4.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day4 {
+	@Test
+	public void WebLoginhomeLoan() 
+	{
+		//selenium	
+		System.out.println("webloginHome");
+	}
+
+	@Test
+	public void MobileLoginhomeLoan() 
+	{
+		//Appium
+		System.out.println("mobileloginHome");
+
+	}
+	
+	@Test
+	public void LoginAPIhomeLoan()
+	{
+		// Rest API 
+
+		System.out.println("restloginHome");
+}
+}
